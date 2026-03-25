12fb98c4c6ee9296d91acb6568444eaebf142e5f
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-03-25 23:28:42 +0300

    Update Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -7,22 +7,22 @@ import { ImageWithFallback } from '../components/figma/ImageWithFallback';
 const galleryImages = [
   {
     id: 1,
-    url: 'https://images.unsplash.com/photo-1548094967-e25a127d1f6d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxyZWQlMjByb3NlcyUyMGJvdXF1ZXR8ZW58MXx8fHwxNzc0MzAwMTMyfDA&ixlib=rb-4.1.0&q=80&w=1080',
+    url: 'https://scontent.famm11-1.fna.fbcdn.net/v/t39.30808-6/657937373_122204919536498856_7735003159805374802_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=13d280&_nc_ohc=WWpm5cVx1roQ7kNvwGqN07q&_nc_oc=Adrmk-cT1DL-4ff-YVcSHq3nkMkQKG9mHkK7Ljxgezz2vsS9LPvMHYdW3N6qnoOOqfGzEYLMePsAHwO_80Yz23rM&_nc_zt=23&_nc_ht=scontent.famm11-1.fna&_nc_gid=4neRbA1GoybKeq0iQlX8Sg&_nc_ss=7a32e&oh=00_AfwowkJh7l0FDmLmqly8fXzzI5s7q3KvVTS8YtE9lr0qVg&oe=69CA003F',
     alt: 'Red roses bouquet',
   },
   {
     id: 2,
-    url: 'https://images.unsplash.com/photo-1696613755401-dac200797436?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwaW5rJTIwcm9zZXMlMjBmbG93ZXJzfGVufDF8fHx8MTc3NDI5NzYyOXww&ixlib=rb-4.1.0&q=80&w=1080',
+    url: 'https://scontent.famm13-1.fna.fbcdn.net/v/t39.30808-6/656848752_122204919824498856_1311471240225801069_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=13d280&_nc_ohc=GMARM0qyPtkQ7kNvwFXeeOE&_nc_oc=AdoD9c-IsMbuy_FxZd9dn_aJ6-0kjnOcIs4lNKsLgKWF-acLj-uwwfwxlexnSmhjidOd5nhNq4974Rhcle4QOkAv&_nc_zt=23&_nc_ht=scontent.famm13-1.fna&_nc_gid=Rt-ScueoXOZ-t3wQ5_vWLQ&_nc_ss=7a32e&oh=00_Afw6iKLWivBJgyenUzg-HbJToRetcAkjiLSDd5xAgKQ5iQ&oe=69CA2568',
     alt: 'Pink roses',
   },
   {
     id: 3,
-    url: 'https://images.unsplash.com/photo-1687308447351-e7997a702e09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx3aGl0ZSUyMHJvc2VzJTIwYXJyYW5nZW1lbnR8ZW58MXx8fHwxNzc0MjgyMzEwfDA&ixlib=rb-4.1.0&q=80&w=1080',
+    url: 'https://scontent.famm13-1.fna.fbcdn.net/v/t39.30808-6/656884151_122204759024498856_7458155976813117729_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=13d280&_nc_ohc=MnEUOGkgjYoQ7kNvwHeJMPp&_nc_oc=AdrPrrkk0_kHz6PV5eV9W1Mj4QkR0nmhTskvEd9KF-sKmzGgJci4Wkz3JjFLNUkYO7M9bB23DTqV4vOtSaCUtCML&_nc_zt=23&_nc_ht=scontent.famm13-1.fna&_nc_gid=pJ0-OpptriaFnCYRtLewQQ&_nc_ss=7a32e&oh=00_Afzb3r6WiTBr3mDALz3C5zYVbSYulkZd0eVGHVxNdRDM-w&oe=69CA2AD3',
     alt: 'White roses arrangement',
   },
   {
     id: 4,
-    url: 'https://images.unsplash.com/photo-1719538832618-cd1d30219ee5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx5ZWxsb3clMjByb3NlcyUyMGdhcmRlbnxlbnwxfHx8fDE3NzQzODg2MjB8MA&ixlib=rb-4.1.0&q=80&w=1080',
+    url: 'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/656492395_122204759018498856_6324798208926009721_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=13d280&_nc_ohc=sRIliObMpEkQ7kNvwFO0qVt&_nc_oc=Adoy_Ry9COOibLTSNr-SkBn6Rogf_qKgrYsKxUvn6wd8Y7ekqMNvPyLejP-TtcHX7g1M6rHBWK5F_p_xJuKxT5vT&_nc_zt=23&_nc_ht=scontent.famm10-1.fna&_nc_gid=qHHYGaWfCEw2MOp_Jzo0iQ&_nc_ss=7a32e&oh=00_AfwXGQePRxvgr_WSxummdeYmOR4qk2oCiIynOCJqB-ScRg&oe=69CA0A8E',
     alt: 'Yellow roses',
   },
 ];
@@ -36,7 +36,7 @@ export function Home() {
       <section className="relative h-[500px] md:h-[600px] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0">
           <ImageWithFallback
-            src="https://images.unsplash.com/photo-1719586901803-d3a5ef35b28f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxmbG93ZXIlMjBzaG9wJTIwaW50ZXJpb3J8ZW58MXx8fHwxNzc0MzUzMzU4fDA&ixlib=rb-4.1.0&q=80&w=1080"
+            src="https://scontent.famm7-1.fna.fbcdn.net/v/t39.30808-6/502468159_122161968014498856_7393435856732539390_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=53a332&_nc_ohc=PsVGcFWT5YAQ7kNvwG32zdV&_nc_oc=AdrF1l8EPLSO9L2je5fHUfnYfSrnGKQbWFNAowN56Xqnowmra2UFSI6BUmkrmV5Bd5qIG9Yu5_TVm34IERWQaPaP&_nc_zt=23&_nc_ht=scontent.famm7-1.fna&_nc_gid=10w_cL8zKMrlUIi7BJYN5Q&_nc_ss=7a30f&oh=00_Afwv9WWcLG2HWYxqzRm-RLcutIns7cbC9XU6eaJLS9PO6A&oe=69CA11A8"
             alt="Flower shop"
             className="w-full h-full object-cover"
           />
